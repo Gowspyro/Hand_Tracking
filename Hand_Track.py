3b63f00a89397d5334cb9f969bee66b67ad3de9c
Gauvrang Ade <72305019+Gowspyro@users.noreply.github.com>
2024-07-10 20:16:52 +0530

    Update Hand_Track.py

--- Hand_Track.py
+++ Hand_Track.py
@@ -1,5 +1,35 @@
-import mediapipe as mp 
-import time 
+import cv2
+import mediapipe as mp
+import time
+
+cap = cv2.VideoCapture(0)#Put your cam no. here, Zero if defaault webcam is used
+
+mpHands = mp.solutions.hands
+hands = mpHands.Hands()
+mpDraw = mp.solutions.drawing_utils
+
+pTime = 0
+cTime = 0
+
+while True:
+    success, img = cap.read()
+    imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    results = hands.process(imgRGB)
+    # print(results.multi_hand_landmarks)
+
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            for id, lm in enumerate(handLms.landmark):
+                # print(id, lm) prints the location of all ids in the terminal every second
+                h, w, c = img.shape
+                cx, cy = int(lm.x * w), int(lm.y * h)
+                print(id, cx, cy)
+                # if id == 4: This specifies the landmark to get only that value 
+                cv2.circle(img, (cx, cy), 15, (255, 0, 255), cv2.FILLED)
+
+            mpDraw.draw_landmarks(img, handLms, mpHands.HAND_CONNECTIONS) #Draw connections between all the points
+
+
 
 
 
